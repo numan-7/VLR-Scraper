4e1614c16be74a027ff3104adfd4b63f9e917782
Numan Khan <101899800+numan-7@users.noreply.github.com>
2023-11-22 11:42:20 -0800

    Fixed get_full_quote to work for all cases

--- spider/spiders/vlr.py
+++ spider/spiders/vlr.py
@@ -52,17 +52,15 @@ class UserPostsSpider(scrapy.Spider):
 
             self.user_item['upvotes'] += upvote_count
             self.user_item['downvotes'] += downvote_count
-
-            text_content = "./ancestor::div[contains(@class, 'wf-card post')]/div[contains(@class, 'post-body')]/p"
             
             if upvote_count > self.user_item['biggest_upvote']:
                 self.user_item['biggest_upvote'] = upvote_count
                 self.user_item['biggest_upvote_url'] = self.get_full_url(post_author, post_url_xpath, response)
-                self.user_item['biggest_upvote_quote'] = self.get_full_quote(post_author, text_content)
+                self.user_item['biggest_upvote_quote'] = self.get_full_quote(post_author)
             if downvote_count < self.user_item['biggest_downvote']:
                 self.user_item['biggest_downvote'] = downvote_count
                 self.user_item['biggest_downvote_url'] = self.get_full_url(post_author, post_url_xpath, response)
-                self.user_item['biggest_downvote_quote'] = self.get_full_quote(post_author, text_content)
+                self.user_item['biggest_downvote_quote'] = self.get_full_quote(post_author)
 
         yield self.user_item
         
@@ -70,23 +68,14 @@ class UserPostsSpider(scrapy.Spider):
         for link in continue_links:
             yield response.follow(link, self.parse_discussion)
 
-    def get_full_quote(self, post_author, text_content):
-        p_tags = post_author.xpath(text_content)
-        # if top comment is in a spoiler div ^-^
-        if not p_tags:
-            text_content = "./ancestor::div[contains(@class, 'wf-card post')]/div[contains(@class, 'post-body')]//div[contains(@class, 'spoiler js-post-spoiler')]/p"
-            p_tags = post_author.xpath(text_content)
-        all_text_contents = []
-        for p in p_tags:
-            # pirect text 
-            direct_text = p.xpath("text()").get()
-            if direct_text:
-                all_text_contents.append(direct_text)
-            # text from <br> elements...please be only edge case im giong to kms
-            br_texts = p.xpath("./br/following-sibling::text()").getall()
-            all_text_contents.extend(br_texts)
-            
-        return " ".join(all_text_contents)
+    def get_full_quote(self, post_author):
+        # get content of the div that holds the users post
+        post_body = post_author.xpath("./ancestor::div[contains(@class, 'wf-card post')]/div[contains(@class, 'post-body')]")
+        # extract all text nodes
+        text_nodes = post_body.xpath(".//text()").getall()
+        # concate them
+        full_text_content = " ".join(text_nodes).strip()
+        return full_text_content
     
     def get_full_url(self, post_author, post_url_xpath, response):
         post_url = post_author.xpath(post_url_xpath).get()
